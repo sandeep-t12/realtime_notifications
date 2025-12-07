32be15ccedd107288c08798dbf78beabbc5d229b
Sandeep <sandeep.thummala12@gmail.com>
2025-12-07 20:25:36 +0530

    django notification model is created & migrated to postgre

---
+++ notifications/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
+class Notification(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='notifications')
+    message = models.CharField(max_length=255)
+    is_read = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['-created_at']
+    
+    def __str__(self):
+        return f"{self.user} - {self.message[:30]}"
